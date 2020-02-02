1aaf99763e857bfaa0c3e2d1866fa4422eb243c1
aniroxxsc <aniroxxtheworld@gmail.com>
2020-02-02 09:51:13 +0530

    ravi sir submission

---
+++ untitled folder/Combo.java
@@ -0,0 +1,13 @@
+class Combo{
+	public static void main(String args[]){
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		System.out.println(""+ a+ b+c);
+		System.out.println(""+a+b+c);
+		System.out.println(""+b+ a+ c);
+		System.out.println(""+b+ c+ a);
+		System.out.println(""+c+ a+ b);
+		System.out.println(""+c+ b+ a);
+	}
+}
